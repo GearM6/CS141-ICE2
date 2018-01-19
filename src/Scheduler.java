96c79f1773b3c634b5fb9fe0329cffa00421bbbd
LamLieu <LamLieu@users.noreply.github.com>
2018-01-19 15:09:22 -0800

    Cleaned up code.

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -6,13 +6,12 @@ import java.util.*;
 public class Scheduler {
 
 	public static void main(String[] args) {
-		int sectionNumber;
 		String courseName;
 		String dates;
 		int startTime;
 		int endTime;
 
-		String askAddCourse = "n";
+		String askAddCourse;
 
 		boolean courseChecker = false;
 		Scanner kb = new Scanner(System.in);
@@ -23,42 +22,42 @@ public class Scheduler {
 			courseName = kb.nextLine();
 			switch (courseName.toLowerCase())
 			{
-				case cs141: {
-					Course cs141 = new Course(CS141, 0, 10, 11);
+				case "cs141": {
+					Course cs141 = new Course(courseName, 0, 10, 11);
 					dates = cs141.getDatesVerbose();
 					startTime = cs141.getStartTime();
 					endTime = cs141.getEndTime();
 					courseName = cs141.getCourseName();
 					System.out.print("Would you like to add another class?(y/n)");
 					askAddCourse = kb.nextLine();
-					if (addAnotherCourse.toLowerCase() != y || addAnotherCourse.toLowerCase != n)
+					if (askAddCourse.charAt(0) != 'y' || askAddCourse.charAt(0) != 'n')
 						courseChecker = true;
 				}
 
-				case eng: {
-					Course eng = new Course(English, 1, 11, 12);
+				case "eng": {
+					Course eng = new Course(courseName, 1, 11, 12);
 					dates = eng.getDatesVerbose();
 					startTime = eng.getStartTime();
 					endTime = eng.getEndTime();
 					courseName = eng.getCourseName();
 					System.out.print("Would you like to add another class?(y/n)");
-					addAnotherCourse = kb.nextLine();
-					if (addAnotherCourse.toLowerCase() != y || addAnotherCourse.toLowerCase != n)
+					askAddCourse = kb.nextLine();
+					if (askAddCourse.charAt(0) != 'y' || askAddCourse.charAt(0) != 'n')
 						courseChecker = true;
 				}
 
-				case bio: {
-					Course bio = new Course(Biology, 1, 9, 10);
+				case "bio": {
+					Course bio = new Course(courseName, 1, 9, 10);
 					dates = bio.getDatesVerbose();
 					startTime = bio.getStartTime();
 					endTime = bio.getEndTime();
 					courseName = bio.getCourseName();
 					System.out.print("Would you like to add another class?(y/n)");
-					addAnotherCourse = kb.nextLine();
-					if (addAnotherCourse.toLowerCase() != y || addAnotherCourse.toLowerCase != n)
+					askAddCourse = kb.nextLine();
+					if (askAddCourse.charAt(0) != 'y' || askAddCourse.charAt(0) != 'n')
 						courseChecker = true;
 				}
 			}
-		} while (courseChecker == false);
+		} while (!courseChecker);
 	}
 }
