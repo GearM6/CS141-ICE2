d394bd05e466a2848e1193309dfbd85c9f52e55c
LamLieu <lamlieu@users.noreply.github.com>
2018-01-19 22:12:18 -0800

    Cleaned up some code. Added askCheck method. Added addNewCourse method.

--- Scheduler.java
+++ Scheduler.java
@@ -2,65 +2,98 @@
 THIS IS THE DRIVER FILE
 */
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Scheduler {
+    public static int courseCounter = 0; //Counts Number of Courses
+    public static boolean courseChecker = false; //Determines whether or not the user will add a new course
+    public static String courseName, oldCourseName; //Sets course name and passes old course name
+    public static String askAddCourse;  //Asks if the user would like to add another course
 
     public static void main(String[] args) {
-        String courseName;
-        String dates;
-        int startTime;
-        int endTime;
+        String dates; //
+        int startTime, endTime; //
+        boolean courseChecker = false; //Determines whether or not the user will add a new course
 
-        String askAddCourse;
-        int courseCounter = 0;
-        boolean courseChecker = false;
+        //Passing Variables into the Constructor
+        Course cs141 = new Course(courseName, "0", 10, 11); //Creates CS141 course
+        Course bio = new Course(courseName, "1", 9, 10); //Creates Bio course
+        Course eng = new Course(courseName, "1", 11, 12); //Creates Eng course
+
         Scanner kb = new Scanner(System.in);
-        System.out.println("Welcome to your schedule builder!");
+        System.out.println("Welcome to your schedule builder!\n");
+
+        System.out.print("Please enter a course that you would like to add(CS141, Bio, or Eng): ");
+        courseName = kb.nextLine(); //Gets User Input of which course they would like to add.
 
         do {
-            System.out.print("Please enter a course that you would like to add(CS141, Bio, or Eng): ");
-            courseName = kb.nextLine();
-            switch (courseName.toLowerCase()) {
+            switch (courseName.toLowerCase()) { //Adds courses to schedule (need to cleanup)
                 case "cs141": {
-                    Course cs141 = new Course(courseName, "0", 10, 11);
                     dates = cs141.getDatesVerbose();
                     startTime = cs141.getStartTime();
                     endTime = cs141.getEndTime();
                     courseName = cs141.getCourseName();
-                    System.out.print("Would you like to add another class?(y/n)");
-                    askAddCourse = kb.nextLine();
-                    if (askAddCourse.charAt(0) != 'y' || askAddCourse.charAt(0) != 'n')
-                        courseChecker = true;
+
                     courseCounter++;
+                    System.out.print("Would you like to add another class?(y/n): ");
+                    askAddCourse = kb.nextLine();
+                    askCheck(askAddCourse);
                 }
 
                 case "eng": {
-                    Course eng = new Course(courseName, "1", 11, 12);
                     dates = eng.getDatesVerbose();
                     startTime = eng.getStartTime();
                     endTime = eng.getEndTime();
                     courseName = eng.getCourseName();
-                    System.out.print("Would you like to add another class?(y/n)");
-                    askAddCourse = kb.nextLine();
-                    if (askAddCourse.charAt(0) != 'y' || askAddCourse.charAt(0) != 'n')
-                        courseChecker = true;
+
                     courseCounter++;
+                    System.out.print("Would you like to add another class?(y/n): ");
+                    askAddCourse = kb.nextLine();
+                    askCheck(askAddCourse);
                 }
 
                 case "bio": {
-                    Course bio = new Course(courseName, "1", 9, 10);
                     dates = bio.getDatesVerbose();
                     startTime = bio.getStartTime();
                     endTime = bio.getEndTime();
                     courseName = bio.getCourseName();
-                    System.out.print("Would you like to add another class?(y/n)");
-                    askAddCourse = kb.nextLine();
-                    if (askAddCourse.charAt(0) != 'y' || askAddCourse.charAt(0) != 'n')
-                        courseChecker = true;
+
                     courseCounter++;
+                    System.out.print("Would you like to add another class?(y/n): ");
+                    askAddCourse = kb.nextLine();
+                    askCheck(askAddCourse);
+                }
+
+                default: {
+                    System.out.println("Invalid course name.");
+                    System.exit(0); //Temporary Fix
                 }
             }
-        } while (!courseChecker && courseCounter < 4);
+        } while (courseChecker && courseCounter <= 4);
+    }
+
+    public static boolean askCheck(String charCheck) {
+        Scanner kb = new Scanner(System.in);
+        boolean check = true;
+
+        while (charCheck.charAt(0) != 'y' || charCheck.charAt(0) != 'n') {
+            System.out.println("Error: Please enter y or n.");
+            System.out.print("Would you like to add another class?(y/n): ");
+            askAddCourse = kb.nextLine();
+        }
+
+        if (askAddCourse.charAt(0) == 'y') {
+            addNewCourse();
+        }
+        else
+            check = false;
+        return check;
+    }
+
+    public static void addNewCourse() { //After adding initial course, adds more courses.
+        Scanner kb = new Scanner(System.in);
+        oldCourseName = courseName;
+        System.out.print("Please enter another course that you would like to add(CS141, Bio, or Eng): ");
+        courseName = kb.nextLine();
     }
 }
