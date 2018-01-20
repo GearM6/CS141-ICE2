950f6e6c768f28e7c1f8e7855d2cbcb80d5e3e22
LamLieu <LamLieu@users.noreply.github.com>
2018-01-20 00:34:16 -0800

    Added error messages for start and end times. Created sortSchedule Method(WIP - Schedule not in order by time). *Additions to Blake's code

--- Scheduler.java
+++ Scheduler.java
@@ -1,99 +1,139 @@
+import java.util.*;
+
 /*
 THIS IS THE DRIVER FILE
 */
-
-import java.util.Scanner;
-
 public class Scheduler {
-    public static int courseCounter = 0; //Counts Number of Courses
-    public static boolean courseChecker = false; //Determines whether or not the user will add a new course
-    public static String courseName, oldCourseName; //Sets course name and passes old course name
-    public static String askAddCourse;  //Asks if the user would like to add another course
+
+
+    public static void checkInterference(Course A, Course B, Course C, Course D) {
+        if (A.getDates().equals(B.getDates())) {
+            if (A.getEndTime() > B.getStartTime() || B.getEndTime() > A.getStartTime())
+                System.out.println("Course conflict between " + A.getCourseName() + " and " + B.getCourseName() + ".");
+        }
+        if (A.getDates().equals(C.getDates())) {
+            if (A.getEndTime() > C.getEndTime() || C.getEndTime() > A.getStartTime())
+                System.out.println("Course conflict between " + A.getCourseName() + " and " + C.getCourseName() + ".");
+        }
+        if (A.getDates().equals(D.getDates())) {
+            if (A.getEndTime() > D.getEndTime() || D.getEndTime() > A.getStartTime())
+                System.out.println("Course conflict between " + A.getCourseName() + " and " + D.getCourseName() + ".");
+        }
+        if (B.getDates().equals(C.getDates())) {
+            if (B.getEndTime() > C.getEndTime() || C.getEndTime() > B.getStartTime())
+                System.out.println("Course conflict between " + B.getCourseName() + " and " + C.getCourseName() + ".");
+        }
+        if (B.getDates().equals(D.getDates())) {
+            if (B.getEndTime() > D.getEndTime() || D.getEndTime() > B.getStartTime())
+                System.out.println("Course conflict between " + B.getCourseName() + " and " + D.getCourseName() + ".");
+        }
+        if (C.getDates().equals(D.getDates())) {
+            if (C.getEndTime() > D.getEndTime() || D.getEndTime() > C.getStartTime())
+                System.out.println("Course conflict between " + C.getCourseName() + " and " + D.getCourseName() + ".");
+        }
+    }
+
+    public static void sortSchedule(Course A, Course B, Course C, Course D) {
+        System.out.println("   MWF");
+        System.out.println("---------");
+        if (A.getDates().charAt(0) == 'M') {
+            System.out.printf("%d-%d: %s\n", A.getStartTime(), A.getEndTime(), A.getCourseName());
+        }
+        else if (B.getDates().charAt(0) == 'M') {
+            System.out.printf("%d-%d: %s\n", B.getStartTime(), B.getEndTime(), B.getCourseName());
+        }
+        else if (C.getDates().charAt(0) == 'M') {
+            System.out.printf("%d-%d: %s\n", C.getStartTime(), C.getEndTime(), C.getCourseName());
+        }
+        else if (D.getDates().charAt(0) == 'M') {
+            System.out.printf("%d-%d: %s\n", D.getStartTime(), D.getEndTime(), D.getCourseName());
+        }
+        System.out.println();
+
+        System.out.println("   TTH");
+        System.out.println("---------");
+        if (A.getDates().charAt(0) == 'T') {
+            System.out.printf("%d-%d: %s\n", A.getStartTime(), A.getEndTime(), A.getCourseName());
+        }
+        else if (B.getDates().charAt(0) == 'T') {
+            System.out.printf("%d-%d: %s\n", B.getStartTime(), B.getEndTime(), B.getCourseName());
+        }
+        else if (C.getDates().charAt(0) == 'T') {
+            System.out.printf("%d-%d: %s\n", C.getStartTime(), C.getEndTime(), C.getCourseName());
+        }
+        else if (D.getDates().charAt(0) == 'T') {
+            System.out.printf("%d-%d: %s\n", D.getStartTime(), D.getEndTime(), D.getCourseName());
+        }
+    }
 
     public static void main(String[] args) {
-        String dates; //
-        int startTime, endTime; //
-        boolean courseChecker = false; //Determines whether or not the user will add a new course
+        String courseName, dates = null;
+        int startTime = 1, endTime = 0, input;
+        Scanner scan = new Scanner(System.in);
+        Course course1 = null;
+        Course course2 = null;
+        Course course3 = null;
+        Course course4 = null;
+        for (int i = 0; i <= 3; i++) {
 
-        //Passing Variables into the Constructor
-        Course cs141 = new Course(courseName, "0", 10, 11); //Creates CS141 course
-        Course bio = new Course(courseName, "1", 9, 10); //Creates Bio course
-        Course eng = new Course(courseName, "1", 11, 12); //Creates Eng course
+            System.out.print("Please enter the course name: ");
+            courseName = scan.next();
+            scan.nextLine();
 
-        Scanner kb = new Scanner(System.in);
-        System.out.println("Welcome to your schedule builder!\n");
+            while (true) {
+                System.out.print("Please enter 1 for MWF or 2 for TTH meeting days: ");
+                input = scan.nextInt();
+                scan.nextLine();
+                switch (input) {
+                    case 1:
+                        dates = "MWF";
+                        System.out.println("Date set to MWF");
+                        break;
+                    case 2:
+                        dates = "TTH";
+                        break;
+                    default:
+                        System.out.println("Please enter 1 or 2!");
+                }
+                if (input == 1 || input == 2)
+                    break;
+            }
 
-        System.out.print("Please enter a course that you would like to add(CS141, Bio, or Eng): ");
-        courseName = kb.nextLine(); //Gets User Input of which course they would like to add.
-
-        do {
-            switch (courseName.toLowerCase()) { //Adds courses to schedule (need to cleanup)
-                case "cs141": {
-                    dates = cs141.getDatesVerbose();
-                    startTime = cs141.getStartTime();
-                    endTime = cs141.getEndTime();
-                    courseName = cs141.getCourseName();
-
-                    courseCounter++;
-                    System.out.print("Would you like to add another class?(y/n): ");
-                    askAddCourse = kb.nextLine();
-                    askCheck(askAddCourse);
+            while (startTime > endTime) {
+                System.out.print("Please enter the start time (0-24): ");
+                startTime = scan.nextInt();
+                scan.nextLine();
+                if (startTime < 0 || startTime > 24) {
+                    System.out.println("Error: Input is out of bounds.");
+                    System.out.print("Please enter the start time (0-24): ");
+                    startTime = scan.nextInt();
+                    scan.nextLine();
                 }
 
-                case "eng": {
-                    dates = eng.getDatesVerbose();
-                    startTime = eng.getStartTime();
-                    endTime = eng.getEndTime();
-                    courseName = eng.getCourseName();
-
-                    courseCounter++;
-                    System.out.print("Would you like to add another class?(y/n): ");
-                    askAddCourse = kb.nextLine();
-                    askCheck(askAddCourse);
-                }
-
-                case "bio": {
-                    dates = bio.getDatesVerbose();
-                    startTime = bio.getStartTime();
-                    endTime = bio.getEndTime();
-                    courseName = bio.getCourseName();
-
-                    courseCounter++;
-                    System.out.print("Would you like to add another class?(y/n): ");
-                    askAddCourse = kb.nextLine();
-                    askCheck(askAddCourse);
-                }
-
-                default: {
-                    System.out.println("Invalid course name.");
-                    System.exit(0); //Temporary Fix
+                System.out.print("Please enter the end time (0-24): ");
+                endTime = scan.nextInt();
+                scan.nextLine();
+                if (endTime < 0 || endTime > 24) {
+                    System.out.println("Error: Input is out of bounds.");
+                    System.out.print("Please enter the end time (0-24): ");
+                    endTime = scan.nextInt();
+                    scan.nextLine();
                 }
             }
-        } while (courseChecker && courseCounter <= 4);
-    }
 
-    public static boolean askCheck(String charCheck) {
-        Scanner kb = new Scanner(System.in);
-        boolean check = true;
-
-        while (charCheck.charAt(0) != 'y' || charCheck.charAt(0) != 'n') {
-            System.out.println("Error: Please enter y or n.");
-            System.out.print("Would you like to add another class?(y/n): ");
-            askAddCourse = kb.nextLine();
+            if (i == 0)
+                course1 = new Course(courseName, dates, startTime, endTime);
+            else if (i == 1)
+                course2 = new Course(courseName, dates, startTime, endTime);
+            else if (i == 2)
+                course3 = new Course(courseName, dates, startTime, endTime);
+            else if (i == 3)
+                course4 = new Course(courseName, dates, startTime, endTime);
         }
 
-        if (askAddCourse.charAt(0) == 'y') {
-            addNewCourse();
-        }
-        else
-            check = false;
-        return check;
-    }
+        checkInterference(course1, course2, course3, course4);
+        sortSchedule(course1, course2, course3, course4);
 
-    public static void addNewCourse() { //After adding initial course, adds more courses.
-        Scanner kb = new Scanner(System.in);
-        oldCourseName = courseName;
-        System.out.print("Please enter another course that you would like to add(CS141, Bio, or Eng): ");
-        courseName = kb.nextLine();
+        scan.close();
     }
 }
