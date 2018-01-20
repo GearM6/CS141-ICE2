ba0673f3ea7546b9073c07bf630e8e688d4f0b14
Lam Lieu <35049466+LamLieu@users.noreply.github.com>
2018-01-20 11:29:08 -0800

    Added a few things.
    Fixed start and end time error messages.
    Created and still working on sortSchedule method.

--- Scheduler.java
+++ Scheduler.java
@@ -6,7 +6,6 @@ THIS IS THE DRIVER FILE
 public class Scheduler {
 
 
-
     public static void checkInterference(Course A, Course B, Course C, Course D) {
         if (A.getDates().equals(B.getDates())) {
             if (A.getEndTime() > B.getStartTime() || B.getEndTime() > A.getStartTime())
@@ -34,9 +33,64 @@ public class Scheduler {
         }
     }
 
+    public static void sortSchedule(Course A, Course B, Course C, Course D) {
+        String[] sortMWF = new String[3];
+        String[] sortTTH = new String[3];
+
+        if (A.getDates().charAt(0) == 'M')
+            sortMWF[0] = A.getCourseName();
+        else
+            sortTTH[0] = A.getCourseName();
+        if (B.getDates().charAt(0) == 'M')
+            sortMWF[1] = B.getCourseName();
+        else
+            sortTTH[1] = B.getCourseName();
+        if (C.getDates().charAt(0) == 'M')
+            sortMWF[2] = C.getCourseName();
+        else
+            sortTTH[2] = C.getCourseName();
+        if (D.getDates().charAt(0) == 'M')
+            sortMWF[3] = D.getCourseName();
+        else
+            sortTTH[3] = D.getCourseName();
+
+        for (int i = 0; i < sortMWF.length - 1; i++) {
+            String min = A.getCourseName(), temp;
+            for (int j = i + 1; j < sortMWF.length; j++) {
+                if (A.getStartTime()>B.getStartTime())
+                {
+                    min = A.getCourseName();
+                    sortMWF[i] = sortMWF[j];
+                }
+            }
+            temp = sortMWF[i];
+
+        }
+
+        System.out.println("   MWF");
+        System.out.println("---------");
+
+        System.out.println();
+
+        System.out.println("   TTH");
+        System.out.println("---------");
+        if (A.getDates().charAt(0) == 'T') {
+            System.out.printf("%d-%d: %s\n", A.getStartTime(), A.getEndTime(), A.getCourseName());
+        }
+        else if (B.getDates().charAt(0) == 'T') {
+            System.out.printf("%d-%d: %s\n", B.getStartTime(), B.getEndTime(), B.getCourseName());
+        }
+        else if (C.getDates().charAt(0) == 'T') {
+            System.out.printf("%d-%d: %s\n", C.getStartTime(), C.getEndTime(), C.getCourseName());
+        }
+        else if (D.getDates().charAt(0) == 'T') {
+            System.out.printf("%d-%d: %s\n", D.getStartTime(), D.getEndTime(), D.getCourseName());
+        }
+    }
+
     public static void main(String[] args) {
         String courseName, dates = null;
-        int startTime = 1, endTime = 0, input;
+        int startTime = 0, endTime = 1, input;
         Scanner scan = new Scanner(System.in);
         Course course1 = null;
         Course course2 = null;
@@ -67,7 +121,7 @@ public class Scheduler {
                     break;
             }
 
-            while (startTime > endTime) {
+            do {
                 System.out.print("Please enter the start time (0-24): ");
                 startTime = scan.nextInt();
                 scan.nextLine();
@@ -87,7 +141,12 @@ public class Scheduler {
                     endTime = scan.nextInt();
                     scan.nextLine();
                 }
-            }
+
+                if (startTime > endTime) {
+                    System.out.println("Error: Start time is greater than the end time.");
+                    System.out.println("Please reenter start and end times.");
+                }
+            } while (startTime > endTime);
 
             if (i == 0)
                 course1 = new Course(courseName, dates, startTime, endTime);
@@ -100,78 +159,8 @@ public class Scheduler {
         }
 
         checkInterference(course1, course2, course3, course4);
+        sortSchedule(course1, course2, course3, course4);
 
         scan.close();
     }
-}
-
-	public static void checkInterference (Course A, Course B) {
-		if (A.getDates().equals(B.getDates())){
-			if (A.getEndTime() > B.getStartTime())
-			System.out.println("Course conflict between " + A.getCourseName() + " and " + B.getCourseName() + ".");
-		}
-		/*else if (course1.dates.equals(course3.dates)){
-			if (course1.endTime > course3.endTime)
-			System.out.println("Course conflict between " + course1.getCourseName " and " + course3.getCourseName ".")
-		}*/
-	}
-	@SuppressWarnings("null")
-	public static void main(String[] args) {
-		String courseName, dates = null;
-		int startTime, endTime, input;
-		Scanner scan = new Scanner(System.in);
-		Course course1 = null;
-		Course course2 = null;
-		Course course3 = null;
-		Course course4 = null;
-		for(int i = 0; i <= 1; i++){
-
-		    System.out.print("Please enter the course name: ");
-		    courseName = scan.next();
-
-				while (true) {
-					System.out.print("Please enter 1 for MWF or 2 for TTH meeting days: ");
-					input = scan.nextInt();
-					switch(input)
-					{
-						case 1:
-							dates = "MWF";
-							System.out.println("Date set to MWF");
-							break;
-						case 2:
-							dates = "TTH";
-							break;
-						default:
-							System.out.println("Please enter 1 or 2!");
-					}
-					if (input == 1 || input == 2)
-						break;
-				}
-
-
-		  System.out.print("Please enter the start time (0-24): ");
-		  startTime = scan.nextInt();
-
-			System.out.print("Please enter the end time (0-24): ");
-		  endTime = scan.nextInt();
-
-			if (i == 0)
-				course1 = new Course(courseName, dates, startTime, endTime);
-			else if (i == 1)
-				course2 = new Course(courseName, dates, startTime, endTime);
-			else if (i == 2)
-				course3 = new Course(courseName, dates, startTime, endTime);
-			else if (i == 2)
-				course4 = new Course(courseName, dates, startTime, endTime);
-
-
-		}
-		checkInterference(course1, course2);
-		System.out.println(course1.getCourseName());
-		System.out.println(course2.getCourseName());
-		//System.out.println(course3.getCourseName());
-		//System.out.println(course4.getCourseName());
-
-		scan.close();
-	}
 }
