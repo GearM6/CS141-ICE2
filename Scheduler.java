600966b222a0291e43ce97a348439a88072cc7d7
beaceves <35122250+beaceves@users.noreply.github.com>
2018-01-19 16:17:08 -0800

    Working code

--- Scheduler.java
+++ Scheduler.java
@@ -5,40 +5,62 @@ THIS IS THE DRIVER FILE
 */
 public class Scheduler {
 
+
+
+	@SuppressWarnings("null")
 	public static void main(String[] args) {
-		String courseName, dates;
-		int startTime, endTime;
+		String courseName, dates = null;
+		int startTime, endTime, input = 3;
 		Scanner scan = new Scanner(System.in);
-		String[] course = new String{"course1", "course2", "course3", "course4"};
-		for(int i = 1; i <= 4; i++){
-	    Course course = new Course(i);
-	    System.out.println("Please enter the course name: ");
-	    courseName = scan.next();
+		Course course1 = null;
+		Course course2 = null;
+		Course course3 = null;
+		Course course4 = null;
+		for(int i = 0; i <= 3; i++){
+		    //Course course = new Course(i);
+		    System.out.print("Please enter the course name: ");
+		    courseName = scan.next();
 
-			do {
-				System.out.print("Please enter 1 for MWF or 2 for TTH meeting days: ");
-				int input = scan.nextInt();
-				switch(input)
-				{
-					case 1:
-						dates = "MWF";
-						break;
-					case 2:
-						dates = "TTH";
-						break;
-					default:
-						System.out.println("Please enter 1 or 2!");
+				if (input != 1 || input != 2) {
+					System.out.print("Please enter 1 for MWF or 2 for TTH meeting days: ");
+					input = scan.nextInt();
+					switch(input)
+					{
+						case 1:
+							dates = "MWF";
+							System.out.println("Date set to MWF");
+							break;
+						case 2:
+							dates = "TTH";
+							break;
+						default:
+							System.out.println("Please enter 1 or 2!");
+					}
 				}
-				} while (input != 1 || input != 2)
 
 
-	    System.out.print("Please enter the start time (0-24): ");
-	    startTime = scan.nextInt();
+		    System.out.print("Please enter the start time (0-24): ");
+		    startTime = scan.nextInt();
 
 			System.out.print("Please enter the end time (0-24): ");
-	    endTime = scan.nextInt();
-			course[i] = new Course(courseName, dates, startTime, endTime);
-    }
+		    endTime = scan.nextInt();
 
+			if (i == 0)
+				course1 = new Course(courseName, dates, startTime, endTime);
+			else if (i == 1)
+				course2 = new Course(courseName, dates, startTime, endTime);
+			else if (i == 2)
+				course2 = new Course(courseName, dates, startTime, endTime);
+			else if (i == 2)
+				course3 = new Course(courseName, dates, startTime, endTime);
+
+
+		}
+		System.out.println(course1.getCourseName());
+		System.out.println(course2.getCourseName());
+		System.out.println(course3.getCourseName());
+		System.out.println(course4.getCourseName());
+
+		scan.close();
 	}
 }
